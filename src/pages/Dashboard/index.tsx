9600508c4c193648b19fb49e8f0f464fedc5e500
Joao Alves <jp14700@gmail.com>
2021-09-14 19:10:46 -0300

    feat: eslint

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-
 // import { Container } from './styles';
 
 const dashboard: React.FC = () => {
@@ -8,6 +7,6 @@ const dashboard: React.FC = () => {
       <h1>hello world</h1>
     </div>
   );
-}
+};
 
 export default dashboard;
--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -3,7 +3,11 @@ import React from 'react';
 // import { Container } from './styles';
 
 const Login: React.FC = () => {
-  return <div><h1>auth</h1></div>;
-}
+  return (
+    <div>
+      <h1>auth</h1>
+    </div>
+  );
+};
 
 export default Login;
